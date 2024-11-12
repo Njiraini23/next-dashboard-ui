15f6e0f74823e4c09178b931c43d597c434665ed
Njiraini23 <njiraini88@gmail.com>
2024-11-12 15:07:18 +0300

    user card components

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,15 @@
+import Image from "next/image";
+
 const UserCard =({type}:{type:string})=>{
     return (
-        <div className="rounded-2xl odd:bg">UserCard</div>
+        <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
+            <div className="flex justify-between items-center">
+                <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">2024/2025</span>
+                <Image src="/more.png" alt="" width={20} height={20} />
+            </div>
+            <h1>1,344</h1>
+            <h2>{type}</h2>
+        </div>
     )
 }
 
