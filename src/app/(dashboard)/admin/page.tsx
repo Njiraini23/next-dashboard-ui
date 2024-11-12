7d97d6fb19fd1983a581653269c061d6f92d44bb
Njiraini23 <njiraini88@gmail.com>
2024-11-12 14:26:16 +0300

    styling admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,11 @@
 const AdminPage = () => {
     return (
-        <div className="">Admin Page</div>
+        <div className="p-4 gap-4 flex-col md:flex-row">
+            {/* LEFT SIDE */}
+            <div className="w-full lg:w-2/3">L</div>
+            {/*Right*/}
+            <div className="w-full lg:w-1/3">R</div>
+        </div>
     )
 }
 export default AdminPage
