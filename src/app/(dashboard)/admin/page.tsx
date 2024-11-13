862c4ac2c887c913668376fc96ed4e1260389d45
Njiraini23 <njiraini88@gmail.com>
2024-11-13 13:39:39 +0300

    creating the charts

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,11 @@
+import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:flex-row">
             {/* LEFT SIDE */}
-            <div className="w-full lg:w-2/3">
+            <div className="w-full lg:w-2/3 flex flex-col gap-8">
             {/*User Cards*/}
             <div className="flex gap-4 justify-between flex-wrap">
                 <UserCard type="student"/>
@@ -13,8 +14,13 @@ const AdminPage = () => {
                 <UserCard type="staff"/>
             </div>
             {/*MIDDLE CHARTS*/}
-            <div className="flex gap-flex-col lg:flex-row">
-              {/*COUNT CHART */}  
+            <div className="flex gap-4 flex-col lg:flex-row">
+              {/*COUNT CHART */} 
+              <div className="w-full lg:w-1/3 h-[450px]">
+              <CountChart/>
+              </div> 
+              {/*ATTENDANCE CHART*/}
+              <div className="w-full lg:w-2/3 h-[450px]"></div>
             </div>
             {/*BOTTOM CHARTS*/}
             <div className=""></div>
