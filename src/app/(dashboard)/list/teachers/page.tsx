68f18748d1342b48c9f2759939e0b0c766bd8faf
Njiraini23 <njiraini88@gmail.com>
2024-11-22 15:14:19 +0300

    the table contents

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -2,6 +2,19 @@ import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import Image from "next/image";
+import Link from "next/link";
+
+type Teacher = {
+    id:number;
+    teacherId:string;
+    name:string;
+    email?: string;
+    photo:string;
+    phone:string;
+    subjects:string[];
+    classes:string[];
+    address:string;
+    }
 
 const columns = [
     {
@@ -39,7 +52,35 @@ const columns = [
     }
 ];
 
-const TeacherListPage = ()=>{
+const TeacherListPage = ()=> {
+
+    const RenderRow = (item:Teacher)=>{
+        <tr>
+            <td>
+                <Image src={item.photo} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover"
+                />
+                <div className="flex flex-col">
+                    <h3 className="font-semibold">{item.name}</h3>
+                    <p className="text-xs text-gray-500">{item?.email}</p>
+                </div>
+            </td>
+            <td className="hidden md:table-cell">{item.teacherId}</td>
+            <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+            <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+            <td className="hidden md:table-cell">{item.phone}</td>
+            <td className="hidden md:table-cell">{item.address}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                    <Link href={`/list/teachers/${item.id}`}>
+                    <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+                        <Image src="/view.png" alt="" width={16} height={16} />
+                    </button>
+                    </Link>
+                </div>
+            </td>
+        </tr>
+    }
+
     return (
         <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
         {/* TOP */}
