573195c04e7d3212a7bf868bf49a39f0c4d00c61
Njiraini23 <njiraini88@gmail.com>
2024-11-26 17:43:41 +0300

    adding delete and edit on parents page

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -11,7 +11,7 @@ type Parent = {
     name:string;
     email?: string;
     students: string[];
-    phone:string;
+    phone?:string;
     address:string;
     };
 
@@ -44,31 +44,29 @@ const columns = [
 const ParentListPage = ()=> {
 
     const renderRow = (item:Parent)=> (
-        <tr 
-        key={item.id} 
-        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-        >
+        <tr key={item.id} 
+        className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
             <td className="flex items-center gap-4 p-4">
                 <div className="flex flex-col">
                     <h3 className="font-semibold" >{item.name} </h3>
-                    <p className="text-xs text-gray-500"> {item?.email} </p>
+                    <p className="text-xs text-gray-500"> {item?.email}</p>
                 </div>
-                <td>
-                <td className="hidden md:table-cell">{item.students.join(",")}</td>
-                <td className="hidden md:table-cell">{item.phone}</td>
-                <td className="hidden md:table-cell">{item.address}</td>
-                </td>
-                
+            </td>
+            <td className="hidden md:table-cell">{item.students.join(",")}</td>
+            <td className="hidden md:table-cell">{item.phone}</td>
+            <td className="hidden md:table-cell">{item.address}</td>
+            <td>
                 <div className="flex items-center gap-2">
                     <Link href={`/list/teachers/${item.id}`}>
                     <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
                         <Image src="/view.png" alt="" width={16} height={16} />
-                        </button>
+                    </button>
                     </Link>
-                {role === "admin" && (
-                       <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                        <Image src="/delete.png" alt="" width={16} height={16} />
-                        </button>
+                    {role === "admin" && (
+                        // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+                        // <Image src="/view.png" alt="" width={16} height={16} />
+                        // </button>
+                        <FormModel table="student" type="delete" id={item.id} />
                     )}
                     </div>
             </td>
@@ -90,9 +88,10 @@ const ParentListPage = ()=> {
                         <Image src="/sort.png" alt="" width={14} height={14} />
                     </button>
                      {role === "admin" && (
-                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                            <Image src="/plus.png" alt="" width={14} height={14} />   
-                        </button>
+                      //   <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+                       // <Image src="/plus.png" alt="" width={14} height={14} />
+                  //  </button>
+                  <FormModel table="student" type="create" />
                     )}
                 </div>
             </div>
