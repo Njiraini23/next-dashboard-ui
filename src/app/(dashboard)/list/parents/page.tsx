dfa4845a1aa0d2d4a511605bc4e14e666fe6f7e0
Njiraini23 <njiraini88@gmail.com>
2024-11-27 13:46:13 +0300

    adding the delete component to admin pages

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -57,16 +57,11 @@ const ParentListPage = ()=> {
             <td className="hidden md:table-cell">{item.address}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
-                    <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-                        <Image src="/view.png" alt="" width={16} height={16} />
-                    </button>
-                    </Link>
                     {role === "admin" && (
-                        // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                        // <Image src="/view.png" alt="" width={16} height={16} />
-                        // </button>
-                        <FormModel table="student" type="delete" id={item.id} />
+                        <> 
+                        <FormModel table="parent" type="update" data={item} />
+                        <FormModel table="parent" type="delete" id={item.id} />
+                        </>
                     )}
                     </div>
             </td>
@@ -88,10 +83,8 @@ const ParentListPage = ()=> {
                         <Image src="/sort.png" alt="" width={14} height={14} />
                     </button>
                      {role === "admin" && (
-                      //   <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                       // <Image src="/plus.png" alt="" width={14} height={14} />
-                  //  </button>
-                  <FormModel table="student" type="create" />
+                      
+                  <FormModel table="teacher" type="create" />
                     )}
                 </div>
             </div>
