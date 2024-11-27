de2d46e502c69043d5382a4e796603c3a47be9bc
Njiraini23 <njiraini88@gmail.com>
2024-11-27 14:17:36 +0300

    delete and update events table

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -1,3 +1,4 @@
+import FormModel from "@/components/FormModels";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -55,15 +56,11 @@ const EventListPage = ()=> {
             
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
-                    <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-                        <Image src="/view.png" alt="" width={16} height={16} />
-                    </button>
-                    </Link>
                     {role === "admin" && (
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                        <Image src="/edit.png" alt="" width={16} height={16} />
-                    </button>
+                        <> 
+                        <FormModel table="event" type="update" data={item} />
+                        <FormModel table="event" type="delete" id={item.id} />
+                        </>
                     )}
                     </div>
             </td>
@@ -84,10 +81,8 @@ const EventListPage = ()=> {
                     <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
                         <Image src="/sort.png" alt="" width={14} height={14} />
                     </button>
-                     {role === "admin" && (
-                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                        <Image src="/plus.png" alt="" width={14} height={14} />
-                    </button>
+                    {role === "admin" && (
+                      <FormModel table="event" type="create" />
                     )}
                 </div>
             </div>
