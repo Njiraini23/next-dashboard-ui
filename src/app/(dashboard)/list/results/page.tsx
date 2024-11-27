3b1a3f944b547369365a964b31a2b64ace8708bb
Njiraini23 <njiraini88@gmail.com>
2024-11-27 14:21:13 +0300

    adding delete and update on results

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -1,3 +1,4 @@
+import FormModel from "@/components/FormModels";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -62,15 +63,11 @@ const ResultListPage = ()=> {
             <td className="hidden md:table-cell">{item.date}</td>
             <td>
                 <div className="flex items-center gap-2">
-                    <Link href={`/list/teachers/${item.id}`}>
-                    <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-                        <Image src="/view.png" alt="" width={16} height={16} />
-                    </button>
-                    </Link>
-                    {role === "admin" && (
-                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                        <Image src="/edit.png" alt="" width={16} height={16} />
-                    </button>
+                {role === "admin" && (
+                        <> 
+                        <FormModel table="result" type="update" data={item} />
+                        <FormModel table="result" type="delete" id={item.id} />
+                        </>
                     )}
                     </div>
             </td>
@@ -91,10 +88,8 @@ const ResultListPage = ()=> {
                     <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
                         <Image src="/sort.png" alt="" width={14} height={14} />
                     </button>
-                     {role === "admin" && (
-                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                        <Image src="/plus.png" alt="" width={14} height={14} />
-                    </button>
+                    {role === "admin" && (
+                      <FormModel table="result" type="create" />
                     )}
                 </div>
             </div>
