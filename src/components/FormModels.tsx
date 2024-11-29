caa5a2a0d44086a8bb99042e52ebb327b895ac33
Njiraini23 <njiraini88@gmail.com>
2024-11-29 09:49:43 +0300

    adding the edit and delete on all forms

--- src/components/FormModels.tsx
+++ src/components/FormModels.tsx
@@ -41,7 +41,7 @@ const FormModel = ({
         return type === "delete" && id ? (
         <form action="" className="p-4 flex flex-col gap-4">
             <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}? </span>
-            <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none">Delete</button>
+            <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
         </form> 
     ): (
          "create or update form"
