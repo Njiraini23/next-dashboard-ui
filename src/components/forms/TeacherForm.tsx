b27006da6be6092728a7d7a2376a8096c0327458
Njiraini23 <njiraini88@gmail.com>
2024-11-29 10:57:09 +0300

    form validation

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -1,4 +1,25 @@
 "use client"
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+
+const schema = z.object({
+    username: z
+    .string()
+    .min(4, { message: 'Username must be at least 4 characters long' })
+    .max(20, { message: 'Username must be at most 20 characters long' }),
+    email: z.string().email({message:"Invalid email address"}),
+    password:z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long'})
+    .max(20, { message: 'Password must at most be 20 characters long'}),
+    firstName:z.string().min(1, { message: 'First name is required!'}),
+    firstName:z.string().min(1, { message: 'First name is required!'}),
+    firstName:z.string().min(1, { message: 'First name is required!'}),
+    firstName:z.string().min(1, { message: 'First name is required!'}),
+    firstName:z.string().min(1, { message: 'First name is required!'}),
+
+  });
 
 const TeacherForm = ({
     type, 
