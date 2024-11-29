a8f83e2c1c39941fea23a1a54d804fab3f7f63e8
Njiraini23 <njiraini88@gmail.com>
2024-11-29 12:44:04 +0300

    passing the inputs as props

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,7 +1,26 @@
-const InputField = ()=> {
-    return (
-        <div>InputField</div>
-    )
+import { FieldError } from "react-hook-form";
+
+type InputFieldProps = {
+    label:string;
+    type?:string;
+    register:any;
+    name:string;
+    defaultValue?:string;
+    error?:FieldError;
+    inputProps: React.InputHTMLAttributes<HTMLInputElement>;
 }
+
+const InputField = ({
+    label, 
+    type = "text", 
+    register, 
+    name, 
+    defaultValue, 
+    error, 
+    inputProps 
+}: InputFieldProps)=> {
+    return <div>InputField</div>
+    
+};
 
 export default InputField;
