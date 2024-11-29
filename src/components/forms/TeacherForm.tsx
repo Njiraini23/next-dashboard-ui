8b965819d9609b9b8fbbf9b65adb502bb71add99
Njiraini23 <njiraini88@gmail.com>
2024-11-29 11:57:21 +0300

    validating the teachers form

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -14,10 +14,12 @@ const schema = z.object({
     .min(8, { message: 'Password must be at least 8 characters long'})
     .max(20, { message: 'Password must at most be 20 characters long'}),
     firstName:z.string().min(1, { message: 'First name is required!'}),
-    firstName:z.string().min(1, { message: 'First name is required!'}),
-    firstName:z.string().min(1, { message: 'First name is required!'}),
-    firstName:z.string().min(1, { message: 'First name is required!'}),
-    firstName:z.string().min(1, { message: 'First name is required!'}),
+    lastName:z.string().min(1, { message: 'Lastt name is required!'}),
+    phone:z.string().min(1, { message: 'Phone number is required!'}),
+    address:z.string().min(1, { message: 'Address is required!'}),
+    birthday:z.date({ message: 'Your birthday is required!'}),
+    sex: z.enum(["male", "female"], {message: "Sex is required!"}),
+    img:z.instanceof(File, {message: "Image is required!"})
 
   });
 
@@ -28,8 +30,21 @@ const TeacherForm = ({
     type:"create" | "update"; 
     data?: any;
 })=> {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        resolver: zodResolver(schema),
+      });
     return (
-        <form className="">input</form>
+        <form className="flex flex-col gap-8">
+          <h1 className='text-xl font-semibold'>Create a new teacher</h1>
+          <span className= "text-xs text-gray-400 font-medium" >Authentication Information</span>
+          <input type="text" {...register("username")} className='ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm'/>
+          {errors.username?.message && <p>{errors.username?.message.toString()}</p>}
+          <span className='text-xs text-gray-400 font-medium'>Personal Information</span>
+        </form>
     )
 };
 
