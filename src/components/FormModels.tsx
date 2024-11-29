2c8260a9d44c2c529cfc73cb50feaedf79ec4f1c
Njiraini23 <njiraini88@gmail.com>
2024-11-29 15:25:53 +0300

    optimizing the app

--- src/components/FormModels.tsx
+++ src/components/FormModels.tsx
@@ -1,8 +1,25 @@
 "use client"
 
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
-import TeacherForm from "./forms/TeacherForm";
+//import TeacherForm from "./forms/TeacherForm";
+//import StudentForm from "./forms/StudentForm";
+
+const TeacherForm = dynamic(()=>import("./forms/TeacherForm"),{
+  loading: ()=>  <h1>Loading...</h1>
+});
+const StudentForm = dynamic(()=>import("./forms/StudentForm"),{
+    loading: ()=>  <h1>Loading...</h1>
+  });
+  
+const forms: {
+    [key:string]:(type:"create" | "update", data?: any)=>JSX.Element;
+
+} = {
+   teacher: (type, data) => <TeacherForm type={type} data={data} />,
+   student: (type, data) => <StudentForm type={type} data={data} />,
+ };
 
 const FormModel = ({
     table,
@@ -44,10 +61,12 @@ const FormModel = ({
             <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}? </span>
             <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
         </form> 
-    ): (
-         <TeacherForm type="update" data={data} />
+    ): type === "create"  || type === "update" ? (
+         forms[table](type, data)
+    ) : (
+        "Form not found!"
     );
-    }
+}
 
     return (
     <>
